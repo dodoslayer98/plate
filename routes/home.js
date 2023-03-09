87cdbe7002a55472c2f7ee8a378cd12ba220af23
dodoslayer98 <123933669+dodoslayer98@users.noreply.github.com>
2023-03-09 14:58:28 -0500

    deploying to heroku

--- routes/home.js
+++ routes/home.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const passport = require('passport')
 // const PlateUser = require('../models/plateUser')
 const restaurantCtrl = require('../controllers/restaurants')
